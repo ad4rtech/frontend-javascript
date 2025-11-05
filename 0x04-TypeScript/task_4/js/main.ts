3300fe4d4408405fc08123e48750ea349a0a6ce0
Fidel Adar <adarfidel.ogw@gmail.com>
2025-11-05 19:40:22 +0300

    10. Update task_4/js/main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,14 +1,12 @@
-import { Subjects } from "./subjects/Subject";
-import "./subjects/Teacher";
-import "./subjects/Subject";
+import "./subjects/Cpp";
 import "./subjects/Java";
 import "./subjects/React";
 
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+const cpp = new Subjects.Cpp();
+const java = new Subjects.Java();
+const react = new Subjects.React();
 
-export const cTeacher: Subjects.Teacher = {
+const cTeacher: Subjects.Teacher = {
   firstName: "John",
   lastName: "Doe",
   experienceTeachingC: 10,
